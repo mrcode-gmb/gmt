7d2b19fa22751f0826dc1f0eb48e96c5c6645689
Abubakar Ibrhim Umar <abubakaribrahim30012@gmail.com>
2024-02-29 23:57:53 +0100

    game assigment

---
+++ 3mtt-game-assigment/index.js
@@ -0,0 +1,244 @@
+const canvas = document.querySelector('canvas')
+const c = canvas.getContext('2d')
+const scoreNo = document.querySelector('#scoreNo')
+const container = document.querySelector('.container')
+const startGame = document.querySelector('#startGame')
+const totalPoint = document.querySelector('#totalPoint')
+
+canvas.width = innerWidth
+canvas.height = innerHeight
+
+// this is our player class and methods
+class Player {
+  constructor (x, y, radius, color) {
+    this.x = x
+    this.y = y
+    this.radius = radius
+    this.color = color
+  }
+  draw () {
+    c.beginPath()
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+    c.fillStyle = this.color
+    c.fill()
+  }
+}
+
+class Projectile {
+  constructor (x, y, radius, color, velocity) {
+    this.x = x
+    this.y = y
+    this.radius = radius
+    this.color = color
+    this.velocity = velocity
+  }
+  draw () {
+    c.beginPath()
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+    c.fillStyle = this.color
+    c.fill()
+  }
+  update () {
+    this.draw()
+    this.x = this.x + this.velocity.x
+    this.y = this.y + this.velocity.y
+  }
+}
+
+class Enemy {
+  constructor (x, y, radius, color, velocity) {
+    this.x = x
+    this.y = y
+    this.radius = radius
+    this.color = color
+    this.velocity = velocity
+  }
+  draw () {
+    c.beginPath()
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+    c.fillStyle = this.color
+    c.fill()
+  }
+  update () {
+    this.draw()
+    this.x = this.x + this.velocity.x
+    this.y = this.y + this.velocity.y
+  }
+}
+
+const friction = 0.98
+class Particle {
+  constructor (x, y, radius, color, velocity) {
+    this.x = x
+    this.y = y
+    this.radius = radius
+    this.color = color
+    this.velocity = velocity
+    this.alpha = 1
+  }
+  draw () {
+    c.save()
+    c.globalAlpha = this.alpha
+    c.beginPath()
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+    c.fillStyle = this.color
+    c.fill()
+    c.restore()
+  }
+  update () {
+    this.draw()
+    this.velocity.x *= friction
+    this.velocity.y *= friction
+    this.x = this.x + this.velocity.x
+    this.y = this.y + this.velocity.y
+    this.alpha -= 0.01
+  }
+}
+
+const x = canvas.width / 2
+const y = canvas.height / 2
+
+let player = new Player(x, y, 10, 'white')
+let projectiles = []
+let enemies = []
+let particles = []
+
+function init () {
+  player = new Player(x, y, 10, 'white')
+  projectiles = []
+  enemies = []
+  particles = []
+  score = 0
+  scoreNo.innerHTML = score
+}
+
+function spawnEnamies () {
+  setInterval(() => {
+    const radius = Math.random() * (30 - 4) + 4
+
+    let x
+    let y
+    if (Math.random() < 0.5) {
+      x = Math.random() < 0.5 ? 0 - radius : canvas.width + radius
+      y = Math.random() * canvas.height
+      // y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius
+    } else {
+      x = Math.random() * canvas.width
+      y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius
+    }
+    const color = `hsl(${Math.random() * 360}, 50%, 50%)`
+    const angle = Math.atan2(canvas.height / 2 - y, canvas.width / 2 - x)
+    const velocity = {
+      x: Math.cos(angle),
+      y: Math.sin(angle)
+    }
+    enemies.push(new Enemy(x, y, radius, color, velocity))
+
+    console.log(enemies)
+  }, 1000)
+}
+
+let animationId
+let score = 0
+function animate () {
+  animationId = requestAnimationFrame(animate)
+  c.fillStyle = 'rgba(1,1,1, 0.1)'
+  c.fillRect(0, 0, canvas.width, canvas.height)
+  player.draw()
+  particles.forEach((particle, index) => {
+    if (particle.alpha <= 0) {
+      particles.splice(index, 1)
+    } else {
+      particle.update()
+    }
+  })
+  projectiles.forEach((projectile, projectileIndex) => {
+    projectile.update()
+    if (
+      projectile.x + projectile.radius < 0 ||
+      projectile.x - projectile.radius > canvas.width ||
+      projectile.y + projectile.radius < 0 ||
+      projectile.y - projectile.radius > canvas.height
+    ) {
+      setTimeout(() => {
+        projectiles.splice(projectileIndex, 1)
+      }, 0)
+    }
+  })
+
+  enemies.forEach((enemy, index) => {
+    enemy.update()
+    const dist = Math.hypot(player.x - enemy.x, player.y - enemy.y)
+    if (dist - enemy.radius - player.radius < 1) {
+      container.style.display = 'flex'
+      totalPoint.innerHTML = score
+      startGame.innerHTML = 'Restart Game'
+      cancelAnimationFrame(animationId)
+    }
+    projectiles.forEach((projectile, projectileIndex) => {
+      const dist = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y)
+      if (dist - enemy.radius - projectile.radius < 1) {
+        // create explosion particles
+        for (let i = 0; i < enemy.radius * 2; i++) {
+          particles.push(
+            new Particle(
+              projectile.x,
+              projectile.y,
+              Math.random() * 2,
+              enemy.color,
+              {
+                x: (Math.random() - 0.5) * (Math.random() * 8),
+                y: (Math.random() - 0.5) * (Math.random() * 8)
+              }
+            )
+          )
+        }
+        if (enemy.radius - 10 > 5) {
+          // incress our score
+          score += 5
+          scoreNo.innerHTML = score
+          // enemy.radius -= 10
+          gsap.to(enemy, {
+            radius: enemy.radius - 10
+          })
+          setTimeout(() => {
+            projectiles.splice(projectileIndex, 1)
+          }, 0)
+        } else {
+          // incress our score
+          score += 10
+          scoreNo.innerHTML = score
+          setTimeout(() => {
+            enemies.splice(index, 1)
+            projectiles.splice(projectileIndex, 1)
+          }, 0)
+        }
+      }
+    })
+  })
+}
+addEventListener('click', event => {
+  const angle = Math.atan2(
+    event.clientY - canvas.height / 2,
+    event.clientX - canvas.width / 2
+  )
+  const velocity = {
+    x: Math.cos(angle) * 6,
+    y: Math.sin(angle) * 6
+  }
+  projectiles.push(
+    new Projectile(canvas.width / 2, canvas.height / 2, 5, 'white', velocity)
+  )
+
+  let sound = new Audio('9mm-pistol-shoot-short-reverb-7152.mp3')
+  sound.play()
+})
+
+// start geme btn
+
+startGame.addEventListener('click', () => {
+  init()
+  container.style.display = 'none'
+  animate()
+  spawnEnamies()
+})
